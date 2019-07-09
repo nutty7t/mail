6e81a3d1bc9831ad2471fec34d56a99bf94125f2
ナッティ <hello@nutty.cc>
2019-07-08 21:36:33 -0700

    Update envelope in handler hooks

--- server.py
+++ server.py
@@ -1,30 +1,44 @@
 import asyncio
+import email
 
 from aiosmtpd.controller import Controller
 
 
 class MessageHandler:
-
     async def handle_RCPT(self, server, session, envelope, address, rcpt_options):
         # TODO: check delivery rules
         if not address.endswith('@nutty.email'):
-            return '550 Not Relaying to that Email Address'
+            return '550 Email Does Not Exist'
+
+        # update envelope
+        envelope.rcpt_tos.append(address)
+
         return '250 OK'
 
     async def handle_MAIL(self, server, session, envelope, address, mail_options):
         # TODO: validate SPF
         # ...
-        pass
+
+        # update envelope
+        envelope.mail_from = address
+        envelope.mail_options.extend(mail_options)
+
+        return '250 OK'
 
     async def handle_DATA(self, server, session, envelope):
         # TODO: validate DKIM
         # ...
 
         # TODO: deliver message to maildir
-        message = self.prepare_message(session, envelope)
         # ...
 
-        return '250 OK'
+        # DEBUG: preview message
+        message = email.message_from_bytes(envelope.content)
+        print(f'From: {envelope.mail_from}')
+        print(f'To: {", ".join(envelope.rcpt_tos)}')
+        print(message)
+
+        return '250 Message Accepted'
 
 
 async def main():
@@ -33,7 +47,6 @@ async def main():
 
 
 if __name__ == '__main__':
-    print("hello from smtp server")
     event_loop = asyncio.get_event_loop()
     event_loop.create_task(main())
     event_loop.run_forever()
